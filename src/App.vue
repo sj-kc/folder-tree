a3b864548e193f0d6b96f54bbd470f62dc841b35
sungkimcastro <s.kim.castro@gmail.com>
2020-10-03 15:34:52 +0300

    Set env

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Tree />
+  </div>
+</template>
+
+<script>
+import Tree from "./components/Tree";
+
+export default {
+  name: "App",
+  components: {
+    Tree,
+  },
+};
+</script>
+
